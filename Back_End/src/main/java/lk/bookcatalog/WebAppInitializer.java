b337ed1d9d788a8702bb40bcddb7811d64305654
AsiriM2000 <asiriambagahawaththa@gmail.com>
2023-06-24 22:40:23 +0530

    update content

---
+++ Back_End/src/main/java/lk/bookcatalog/WebAppInitializer.java
@@ -0,0 +1,15 @@
+package lk.bookcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Asiri Madhusanka
+ * @since : 0.1.0
+ **/
+@SpringBootApplication
+public class WebAppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
+    }
+}
